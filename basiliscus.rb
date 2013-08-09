71f1d03a3fe868d09ce9206b1f934e0df3f862e8
Rogelio Zarate <i.am@rogeliozarate.com>
2013-08-09 15:28:06 -0500

    Add code to use time stamp as screen shot  file name

--- basiliscus.rb
+++ basiliscus.rb
@@ -6,6 +6,6 @@ driver = Selenium::WebDriver.for :chrome
 driver.navigate.to site
 driver.manage.window.maximize
 sleep 5                                          # required to wait until scripts and images are loaded
-driver.save_screenshot('screenshot.png')
+screenshot_time = Time.now.strftime("%Y-%m-%dT%H-%M-%S")
+driver.save_screenshot("#{screenshot_time}.png")
 driver.quit
- 
